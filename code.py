7c469b51ab543ada4a49fa1b9b089ac2143d4b00
big NY <Yelnikov@fieldengineer.com>
2019-01-12 04:14:05 +0200

    Update

--- code.py
+++ code.py
@@ -7,71 +7,60 @@ threadAmount = int(sys.argv[1])
 
 
 temp = 0
-#while temp < 10:
 
 noneWorking = [0, 503, 4939, 4940, 4941, 12003, 5556, 5082]
 
-
 # Create target Directory if don't exist
-dirName = 'img'
-if not os.path.exists(dirName):
-    os.mkdir(dirName)
-    print("Directory " , dirName ,  " Created ")
+imgDir = 'img'
+if not os.path.exists(imgDir):
+    os.mkdir(imgDir)
+    print("Directory " , imgDir ,  " Created ")
 else:    
-    print("Directory " , dirName ,  " already exists")
+    print("Directory " , imgDir ,  " already exists")
 
-os.chdir(os.getcwd() + '/'+dirName+'/') 
+os.chdir(os.getcwd() + '/'+imgDir+'/') 
 
 
 def scrapePictures():
-	while True:
-	#	N = int(''.join(random.choice('1' + '2') for _ in range(1)))
-		amount = int(''.join(random.choice('5' + '6') for _ in range(1)))
-		if amount == 6:
-	#		N = int(''.join(random.choice('3') for _ in range(1)))
-			N = 3
-			#name = str(len([name for name in os.listdir('.') if os.path.isfile(name)]) - 1)
-			picture = str(''.join(random.choice(string.ascii_uppercase + string.digits + string.lowercase) for _ in range(N)))
-			picture2 = str(''.join(random.choice(string.digits + string.lowercase) for _ in range(N)))
-			# printsc = "http://img.prntscr.com/img?url=http://i.imgur.com/" + "" + str(picture) + str(picture2) + ".jpg"
-				# Trying to improve.
-			name = picture + picture2
-			printsc = "http://i.imgur.com/" + "" + str(picture) + str(picture2) + ".jpg"
-			urllib.urlretrieve(""+ printsc, str(name) + ".jpg")
-			file = os.path.getsize(str(name)+ ".jpg")
-			# print printsc
-				# original print file. Currently in maintance mode.
-			# print str(file) + " file" #Used to DEBUG and find the none working pictures
-			if file in noneWorking:
-				print "[-] Invalid: " + picture + picture2
-				os.remove(name + ".jpg")
-			else:
-				print "[+] Valid: " + printsc
-			#temp += 1
-		if amount == 5:
-			N = 5
-	#		N = int(''.join(random.choice('3') for _ in range(1)))
-	#		N2 = int(''.join(random.choice('2') for _ in range(1)))
-			#name = str(len([name for name in os.listdir('.') if os.path.isfile(name)]) - 1)
-			picture = str(''.join(random.choice(string.ascii_uppercase + string.digits + string.lowercase) for _ in range(N)))
-	#		picture2 = str(''.join(random.choice(string.digits + string.lowercase) for _ in range(N2)))
-			# printsc = "http://img.prntscr.com/img?url=http://i.imgur.com/" + "" + str(picture) + ".jpg"
-				# Trying to improve.
-			printsc = "http://i.imgur.com/" + "" + str(picture) + ".jpg"
-	#		printsc = "http://img.prntscr.com/img?url=http://i.imgur.com/" + "" + str(picture) + str(picture2) + ".jpg" #Porsiacaso necesito dividr 3 mixed y luego 2 numeros y minusculas
-			name = picture
-			picname = str(name) + ".jpg"
+    while True:
+        # N = int(''.join(random.choice('1' + '2') for _ in range(1)))
+        amount = int(''.join(random.choice('5' + '6') for _ in range(1)))
 
-			urllib.urlretrieve(" "+printsc, picname)
-			file = os.path.getsize(str(name)+ ".jpg")
-			#print printsc
-			# print str(file) + " file" #Used to DEBUG and find the none working pictures
-			if file in noneWorking:
-				print "[-] Invalid: " + picture
-				os.remove(name + ".jpg")
-			else:
-				print "[+] Valid: " + printsc
-			#temp += 1
+        N = 5
+        picture2 = ''
+        breakLine = ''
+
+        if amount == 6:
+            N = 3
+            picture2 = str(''.join(random.choice(string.digits + string.lowercase) for _ in range(N)))
+            breakLine = '\n'
+        picture = str(''.join(random.choice(string.ascii_uppercase + string.digits + string.lowercase) for _ in range(N)))
+        name = str(picture) + str(picture2)
+        printsc = "http://i.imgur.com/" + name + ".jpg"
+        #jpg_name = "%s/%s.jpg" % (dirName, name)
+        jpg_name = name + ".jpg"
+        png_name = name + ".png"
+        gif_name = name + ".gif"
+
+        urllib.urlretrieve(printsc, jpg_name)
+
+        f = open(jpg_name, 'r')
+        flines = f.read().splitlines()
+        fsize = os.path.getsize(jpg_name)
+
+        if (fsize in noneWorking) or (flines[-1] == '</html>'):
+            print "[-] Invalid: " + jpg_name + breakLine
+            os.remove(jpg_name)
+        else:
+            if bytes(flines[0]) == '\x89PNG':
+                os.rename(jpg_name, png_name)
+                print "[+] Valid: " + png_name
+            elif flines[0].startswith('GIF89'):
+                os.rename(jpg_name, gif_name)
+                print "[+] Valid: " + gif_name
+            else:
+                print "[+] Valid: " + jpg_name
+
 
 tempVar2 = 1
 #threadAmount = sys.argv[2]
